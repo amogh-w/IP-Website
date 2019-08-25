1ea9880081ca6ebcc5f50f487a409b980bfdd3e8
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-08-25 18:20:16 +0530

    Select Room: Insertion into DB complete

--- select.php
+++ select.php
@@ -1,3 +1,25 @@
 <?php
-    echo $_POST['roomName'];
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "sunrisedb";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if ( isset($_POST['name']) && isset($_POST['address']) && isset($_POST['zip']) && isset($_POST['number']) && isset($_POST['roomName']) ) {
+        $entered_name = $_POST['name'];
+        $entered_address = $_POST['address'];
+        $entered_zip = $_POST['zip'];
+        $entered_number = $_POST['number'];
+        $selected_roomName = $_POST['roomName'];
+        $id = rand(100, 999);
+
+        $sql = "INSERT INTO request(id, name, address, zipcode, phone, room) VALUES ($id, '$entered_name', '$entered_address', $entered_zip, $entered_number, '$selected_roomName')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
 ?>
