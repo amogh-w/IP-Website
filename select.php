06da51224a5edd4468683d199047bd129da28fa6
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-10-01 15:49:32 +0530

    Project: Complete

--- select.php
+++ select.php
@@ -21,9 +21,42 @@
         $sql = "INSERT INTO request(id, name, address, zipcode, phone, room, status) VALUES ($id, '$entered_name', '$entered_address', '$entered_zip', '$entered_number', '$selected_roomName', '$request_status')";
 
         if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            header("Location: request_success.html");
         }
     }
 ?>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Login</title>
+    <!--Font-->
+    <link
+      href="//fonts.googleapis.com/css?family=Raleway:400,300,600"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <!--Favicon-->
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <!--CSS-->
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/skeleton.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <!--jQuery-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!--Scripts-->
+    <script src="js/custom.js"></script>
+  </head>
+  <body>
+    <div class="login-error-container container">
+        <h3>Error, please try filling the form with correct values!</h3>
+        <a href="login.html"><i class="fa fa-arrow-left" aria-hidden="true"></i> Retry</a>
+    </div>
+  </body>
+</html>
